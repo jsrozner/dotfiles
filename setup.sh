b95729bdebc42a3056a767d50f04d2e9e59ac584
Josh Rozner <jsrozner@gmail.com>
2013-08-31 15:05:28 -0400

    added no-overwrite as default

--- setup.sh
+++ setup.sh
@@ -5,7 +5,12 @@ echo Linking files to home directory
 for file in .
 do
    if [ -f $file ]; then
-      echo Linking $file
-      ln -s $file ~/.$file
+      if [ ! -f $file ]; then
+         echo Linking $file
+         ln -s $file ~/.$file
+      else
+         echo $file already exists in home directory
+         echo remove it to overwrite
+      fi
    fi
 done
