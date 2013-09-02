9d14faef431a20eeb5fd2072ab8031ec0eb9c1e9
Josh Rozner <jsrozner@gmail.com>
2013-09-01 20:24:09 -0400

    configured setup.sh to push keyremap to correct directory

--- setup.sh
+++ setup.sh
@@ -6,3 +6,6 @@ for file in *.sym
 do
    ln -s `pwd`/$file ~/.${file%.sym}
 done
+
+# copy other config files
+# cp `pwd`/other_config/private.xml ~/Library/Application\ Support/KeyRemap4MacBook/private.xml 
